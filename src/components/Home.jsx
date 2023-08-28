ce7ac225155b29f9019d7b678df041f9efc880e9
Andrew Ho <amh7705@psu.edu>
2023-08-28 19:14:21 -0400

    practice pull request

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,7 @@ const Home = () => {
     return (
         <div className="p-16">
             <header className="text-center text-6xl py-52">
-                Agile Aces
+                Agile Aces Game
             </header>
             <Link to="/game" className="flex">
                 <span className="m-auto text-5xl">Click Here To Begin!</span>
