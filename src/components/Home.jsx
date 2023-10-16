6b512e8ba8d0978029cc3d3bea6b713193b34012
Andrew Ho <amh7705@psu.edu>
2023-10-16 17:16:39 -0400

    progress

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,36 +1,13 @@
 import { Link } from "react-router-dom";
-// import { API } from "aws-amplify";
-// const myAPI = "agileAcesAPI";
-// const path = "/gameScores";
 
 const Home = () => {
-    // const init = {
-    //     body: {
-    //         "TableName": "GameScores",
-    //         "Item": {
-    //             "teamName": {
-    //                 "S": "testTeamName",
-    //             }
-    //         }
-    //     }
-    // }
-    // const handleGetGameScores = () => {
-    //     API.post(myAPI, `dev/${path}`, init)
-    //         .then(response => {
-    //             console.log(response);
-    //         })
-    //         .catch(error => {
-    //             console.error("Error getting from DB: ", error);
-    //         });
-    // };
-    // handleGetGameScores();
     return (
         <div className="p-16 text-slate-50">
-            <header className="text-center text-8xl py-48 sm:text-6xl md:text-7xl lg:text-8xl">
+            <header className="text-center py-48 text-6xl md:text-7xl lg:text-8xl">
                 Agile Aces Game
             </header>
             <Link to="/estimate" className="flex">
-                <span className="m-auto text-6xl sm:text-4xl md:text-5xl lg:text-6xl">
+                <span className="m-auto text-4xl md:text-5xl lg:text-6xl">
                     Click Here To Begin!
                 </span>
             </Link>
--- src/components/TimerPage.jsx
+++ src/components/TimerPage.jsx
@@ -16,7 +16,7 @@ const TimerPage = ({ timeInSeconds, soundEnabled }) => {
     const [typeOfTimer] = useContext(TimerContext);
     const navigate = useNavigate();
     const audio = useMemo(() => new Audio(sfx), []);
-    const [, setGameScore] = useContext(GameScoreContext);
+    const [gameScore, setGameScore] = useContext(GameScoreContext);
     const [gameIteration, setGameIteration] = useContext(GameIterationContext);
     const [, setEstimateScore] = useContext(EstimateContext);
     const [teamName, setTeamName] = useContext(TeamNameContext);
@@ -40,34 +40,64 @@ const TimerPage = ({ timeInSeconds, soundEnabled }) => {
         };
     }, [time, navigate, timeInSeconds, audio, soundEnabled]);
 
+    useEffect(() => {
+        let intervalId;
+
+        if (soundEnabled && typeOfTimer === "oneMin" && time !== 0) {
+            intervalId = setInterval(() => {
+                setGameScore((prevGameScore) => {
+                    const updatedGameScore = [...prevGameScore];
+                    updatedGameScore[gameIteration - 1] = {
+                        ...updatedGameScore[gameIteration - 1],
+                        ballsInBox:
+                            updatedGameScore[gameIteration - 1].ballsInBox + 1,
+                    };
+                    return updatedGameScore;
+                });
+            }, 1000);
+        }
+
+        return () => {
+            clearInterval(intervalId);
+        };
+    }, [time, gameIteration, gameScore, soundEnabled, setGameScore, typeOfTimer]);
+
     return (
         <div className="text-center py-40 text-slate-50">
-            <div className="text-9xl">{getTime(time)}</div>
+            <div className="text-7xl md:text-8xl lg:text-9xl">
+                {getTime(time)}
+            </div>
             {!soundEnabled && (
                 <div className="flex flex-col justify-center items-center">
-                    <div className="text-5xl w-4/5 mb-10">
+                    <div className="w-4/5 mb-10 text-3xl md:text-4xl lg:text-5xl">
                         Enter Your Team Name and Estimate How Many Points You
                         Will Score In Iteration 1!
                     </div>
                     <div className="flex flex-row mb-10">
-                        <span className="text-4xl mx-2 mt-2">Team Name:</span>
+                        <span className="mx-2 mt-2 text-2xl md:text-3xl lg:text-4xl">
+                            Team Name:
+                        </span>
                         <input
                             autoComplete="false"
                             type="text"
-                            className="border border-black rounded-lg w-fit h-16 text-center text-3xl "
+                            className="border border-black rounded-lg w-fit h-16 text-center text-2xl md:text-3xl lg:text-4xl"
                             placeholder="Enter Team Name..."
                             value={teamName}
                             onChange={(e) => setTeamName(e.target.value)}
                         />
-                        <span className="text-4xl mx-2 mt-2">
+                        <span className="mx-2 mt-2 text-2xl md:text-3xl lg:text-4xl">
                             Estimated Score:
                         </span>
                         <input
                             autoComplete="false"
                             type="number"
-                            className="border border-black rounded-lg w-44 h-16 text-center text-6xl"
+                            className="border border-black rounded-lg w-44 h-16 text-center text-2xl md:text-3xl lg:text-4xl"
                             onChange={(e) => {
-                                setEstimateScore(isNaN(parseInt(e.target.value)) ? 0 : parseInt(e.target.value));
+                                setEstimateScore(
+                                    isNaN(parseInt(e.target.value))
+                                        ? 0
+                                        : parseInt(e.target.value)
+                                );
                                 setGameScore((prevGameScore) => {
                                     return prevGameScore.map((score, index) => {
                                         if (index === 0) {
@@ -87,7 +117,10 @@ const TimerPage = ({ timeInSeconds, soundEnabled }) => {
                     </div>
                 </div>
             )}
-            <div className="text-6xl"> {time === 0 ? "Time's Up!" : ""}</div>
+            <div className="text-4xl md:text-5xl lg:text-6xl">
+                {" "}
+                {time === 0 ? "Time's Up!" : ""}
+            </div>
             {time === 0 && soundEnabled ? (
                 <Link
                     to="/game"
@@ -105,25 +138,20 @@ const TimerPage = ({ timeInSeconds, soundEnabled }) => {
                                                       ...score,
                                                       delta:
                                                           -1 *
-                                                                (prevGameScore[
-                                                                    index
-                                                                ]
-                                                                    .estimatedScore -
-                                                                    prevGameScore[
-                                                                        index
-                                                                    ]
-                                                                        .ballsInBox +
-                                                                    prevGameScore[
-                                                                        index
-                                                                    ].defects),
+                                                          (prevGameScore[index]
+                                                              .estimatedScore -
+                                                              prevGameScore[
+                                                                  index
+                                                              ].ballsInBox +
+                                                              prevGameScore[
+                                                                  index
+                                                              ].defects),
 
                                                       totalScore:
-                                                          prevGameScore[
-                                                                    index
-                                                                ].ballsInBox -
-                                                                prevGameScore[
-                                                                    index
-                                                                ].defects,
+                                                          prevGameScore[index]
+                                                              .ballsInBox -
+                                                          prevGameScore[index]
+                                                              .defects,
                                                   };
                                               } else {
                                                   return score;
@@ -156,7 +184,12 @@ const TimerPage = ({ timeInSeconds, soundEnabled }) => {
                     Next
                 </Link>
             ) : time === 0 && !soundEnabled ? (
-                <Link to="/game" className="text-5xl px-5">Press Here to Continue!</Link>
+                <Link
+                    to="/game"
+                    className="px-5 text-3xl md:text-4xl lg:text-5xl"
+                >
+                    Press Here to Continue!
+                </Link>
             ) : null}
         </div>
     );
